c1f0a5e86cfbb165d9f22cf061adcc150e69b99b
gusdn0236 <hdno0236@naver.com>
2026-06-12 12:57:25 +0900

    Updates

--- assets/ArtNeon-zJYuDuXH.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-DxTTABX-.js";import{G as t}from"./GalleryPage-iylGcblp.js";const e=[{name:"아크릴네온",subCategory:"아크릴네온",description:"아크릴 소재로 제작된 네온 사인입니다."},{name:"아크릴조각사인",subCategory:"아크릴조각사인",description:"아크릴 조각으로 제작된 사인입니다."}],s=()=>o.jsx(t,{category:"artneon",categoryTabs:e});export{s as default};
